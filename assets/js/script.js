80f3bc476e05892b1c3d17fcf3c7ab7809ec6fb8
Ben-Wheeler333 <chemlovexx@icloud.com>
2022-04-03 22:39:28 -0500

    if/else statements

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,1 +1,30 @@
 $("#currentDay").text(moment());
+ 
+var currentHour = moment().hour();
+var scheduleHour = $(".hour").value;
+
+
+
+    var past = currentHour > scheduleHour;
+    var present = currentHour = scheduleHour;
+    var future = currentHour < scheduleHour;
+
+    var setColor = function() {
+        
+        if (past) {
+            $(".description").addClass("bg-secondary");
+        } else if (future) {
+            $(".description").addClass("bg-success");
+        } else {
+            $(".description").addClass("bg-danger");
+        };
+    }
+
+
+
+$(".saveBtn").on
+
+
+setColor();
+
+
